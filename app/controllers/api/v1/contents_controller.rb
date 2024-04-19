c6d2fd5a62739ef932748bdb3d888ad38042e335
Ashok Poudel <ashokpoudel2051@gmail.com>
2024-04-19 12:40:06 +0545

    feature: add crud operation and routes of contents controller

---
+++ app/controllers/api/v1/contents_controller.rb
@@ -0,0 +1,52 @@
+module Api
+  module V1    
+    class ContentsController < ApplicationController
+      before_action :set_content, only: [:show, :update, :destroy]
+
+      def index
+        @contents = Content.all
+        render json: @contents
+      end
+
+      def show
+        render json: @content
+      end
+
+      def create
+        @content = Content.new(content_params)
+
+        if @content.save
+          render json: @content, status: :created
+        else
+          render json: @content.errors, status: :unprocessable_entity
+        end
+      end
+
+      def update
+        if @content.update(content_params)
+          render json: @content
+        else
+          render json: @content.errors, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        if @content.destroy
+          render json: { message: 'Deleted' }
+        else
+          render json: @content.errors, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def set_content
+        @content = Content.find(params[:id])
+      end
+
+      def content_params
+        params.require(:content).permit(:title, :body)
+      end
+    end
+  end
+end
