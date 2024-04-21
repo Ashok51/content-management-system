27ebfd564df2417803bb8a93630bb6a77674eb28
Ashok Poudel <ashokpoudel2051@gmail.com>
2024-04-21 10:49:56 +0545

    refactor: content spec code refactor

--- spec/controllers/api/v1/contents_controller_spec.rb
+++ spec/controllers/api/v1/contents_controller_spec.rb
@@ -10,8 +10,8 @@ RSpec.describe 'Contents API', type: :request do
   let(:auth_headers1) { user1.create_new_auth_token }
   let(:auth_headers2) { user2.create_new_auth_token }
 
-  context 'index' do
-    it 'logged in user can view all the contents present' do
+  context 'when user is logged in' do
+    it 'allows logged in user to view all the contents present' do
       get '/api/v1/contents',
           headers: { 'Content-Type': 'application/json',
                      'Authorization': auth_headers1['Authorization'] }
@@ -20,18 +20,7 @@ RSpec.describe 'Contents API', type: :request do
       expect(JSON(response.body)['data'].count).to eq(2)
     end
 
-    it 'not signed in user / without having auth token or invalid token' do
-      get '/api/v1/contents',
-          headers: { 'Content-Type': 'application/json',
-                     'Authorization': '' }
-
-      expect(response.status).to eq(401)
-      expect(JSON(response.body)['errors'].first).to eq('You need to sign in or sign up before continuing.')
-    end
-  end
-
-  context 'create' do
-    it 'should create a new content when user is logged in' do
+    it 'allows logged in user to create a new content' do
       post '/api/v1/contents',
            params: { content: { title: 'Newly created Content', body: 'body of content' } }.to_json,
            headers: { 'Content-Type': 'application/json',
@@ -41,43 +30,28 @@ RSpec.describe 'Contents API', type: :request do
       expect(user1.contents.count).to eq(2)
     end
 
-    it 'should not create a new content when user is not signed in' do
-      post '/api/v1/contents',
-           params: { content: { title: 'New Content', body: 'body of content' } }.to_json,
-           headers: { 'Content-Type': 'application/json',
-                      'Authorization': '' }
-
-      expect(response.status).to eq(401)
-      expect(JSON(response.body)['errors'].first).to eq('You need to sign in or sign up before continuing.')
-    end
-  end
-
-  context 'update' do
-    let!(:content) { create(:content, title: 'New Content', user: user1) }
-
-    it 'should update his/her own contents when user is logged in' do
-      put "/api/v1/contents/#{content.id}",
+    it 'allows logged in user to update his/her own contents' do
+      put "/api/v1/contents/#{content1.id}",
           params: { content: { title: 'Updated Content', body: 'Updated body of content' } }.to_json,
           headers: { 'Content-Type': 'application/json',
                      'Authorization': auth_headers1['Authorization'] }
 
       expect(response.status).to eq(200)
       expect(JSON(response.body)['data']['attributes']['title']).to eq('Updated Content')
-      expect(content.reload.title).to eq('Updated Content')
+      expect(content1.reload.title).to eq('Updated Content')
     end
 
-    it 'should not update any of the content when user is not signed in' do
-      put "/api/v1/contents/#{content.id}",
-          params: { content: { title: 'Updated Content', body: 'Updated body of content' } }.to_json,
+    it 'should not update the content when title or body is nil' do
+      put "/api/v1/contents/#{content1.id}",
+          params: { content: { title: 'Updated Content', body: '' } }.to_json,
           headers: { 'Content-Type': 'application/json',
-                     'Authorization': '' }
+                     'Authorization': auth_headers1['Authorization'] }
 
-      expect(response.status).to eq(401)
-      expect(JSON(response.body)['errors'].first).to eq('You need to sign in or sign up before continuing.')
-      expect(content.reload.title).to eq('New Content')
+      expect(JSON(response.body)["body"].first).to eq("can't be blank")
     end
 
-    it 'should not update the contents of others despite logged in' do
+    it 'should not update the contents created by other users' do
+      content_title = content1.title
       put "/api/v1/contents/#{content1.id}",
           params: { content: { title: 'Updated Content', body: 'Updated body of content' } }.to_json,
           headers: { 'Content-Type': 'application/json',
@@ -85,34 +59,20 @@ RSpec.describe 'Contents API', type: :request do
 
       expect(response.status).to eq(401)
       expect(JSON(response.body)['error']).to eq('Unauthorized')
-      expect(content.reload.title).to eq('New Content')
+      expect(content1.reload.title).to eq(content_title)
     end
-  end
 
-  context 'destroy' do
-    let!(:content) { create(:content, title: 'New Content', user: user1) }
-
-    it 'should delete his/her own content when user is logged in' do
-      delete "/api/v1/contents/#{content.id}",
+    it 'should delete his/her own content' do
+      delete "/api/v1/contents/#{content1.id}",
              headers: { 'Content-Type': 'application/json',
                         'Authorization': auth_headers1['Authorization'] }
 
       expect(response.status).to eq(200)
       expect(JSON(response.body)['message']).to eq('Deleted')
-      expect(Content.find_by(id: content.id)).to be_nil
+      expect(Content.find_by(id: content1.id)).to be_nil
     end
 
-    it 'should not delete any of the content when user is not signed in' do
-      delete "/api/v1/contents/#{content.id}",
-             headers: { 'Content-Type': 'application/json',
-                        'Authorization': '' }
-
-      expect(response.status).to eq(401)
-      expect(JSON(response.body)['errors'].first).to eq('You need to sign in or sign up before continuing.')
-      expect(Content.find_by(id: content.id)).not_to be_nil
-    end
-
-    it 'should not delete the contents of others despite logged in' do
+    it 'should not delete the contents created by other users' do
       delete "/api/v1/contents/#{content1.id}",
              headers: { 'Content-Type': 'application/json',
                         'Authorization': auth_headers2['Authorization'] }
@@ -122,4 +82,47 @@ RSpec.describe 'Contents API', type: :request do
       expect(Content.find_by(id: content1.id)).not_to be_nil
     end
   end
+
+  context 'when user is not logged in' do
+    it 'does not allow not signed in user to view contents' do
+      get '/api/v1/contents',
+          headers: { 'Content-Type': 'application/json',
+                     'Authorization': '' }
+
+      expect(response.status).to eq(401)
+      expect(JSON(response.body)['errors'].first).to eq('You need to sign in or sign up before continuing.')
+    end
+
+    it 'does not allow not signed in user to create a new content' do
+      post '/api/v1/contents',
+           params: { content: { title: 'New Content', body: 'body of content' } }.to_json,
+           headers: { 'Content-Type': 'application/json',
+                      'Authorization': '' }
+
+      expect(response.status).to eq(401)
+      expect(JSON(response.body)['errors'].first).to eq('You need to sign in or sign up before continuing.')
+    end
+
+    it 'does not allow not signed in user to update any content' do
+      put "/api/v1/contents/#{content1.id}",
+          params: { content: { title: 'Updated Content', body: 'Updated body of content' } }.to_json,
+          headers: { 'Content-Type': 'application/json',
+                     'Authorization': '' }
+
+      expect(response.status).to eq(401)
+      expect(JSON(response.body)['errors'].first).to eq('You need to sign in or sign up before continuing.')
+      expect(content1.reload.title).not_to eq('Updated Content')
+    end
+
+    it 'does not allow to delete any content' do
+      delete "/api/v1/contents/#{content1.id}",
+             headers: { 'Content-Type': 'application/json',
+                        'Authorization': '' }
+
+      expect(response.status).to eq(401)
+      expect(JSON(response.body)['errors'].first).to eq('You need to sign in or sign up before continuing.')
+      expect(Content.find_by(id: content1.id)).not_to be_nil
+    end
+  end
 end
+
