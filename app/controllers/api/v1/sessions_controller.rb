1ab337132eb396a17b23f67828383fafb3ebd371
Ashok Poudel <ashokpoudel2051@gmail.com>
2024-04-21 13:33:06 +0545

    feature: add token as the user attribute response, after sign in and pass it through serializer

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,18 +1,29 @@
+# frozen_string_literal: true
+
 module Api
   module V1
     class SessionsController < DeviseTokenAuth::SessionsController
-      # Override the method
-      def render_create_success
-        render json: @resource,
-               serializer: UserSerializer,
-               adapter: :json_api
+      def create
+        super do |resource|
+          if resource && response.status == 200
+            render json: resource, serializer: UserSerializer,
+                   authorization_token: auth_headers(resource)['Authorization'],
+                   adapter: :json_api
+
+            return
+          end
+        end
       end
 
       private
 
       def resource_params
         auth = params[:auth] || params[:session][:auth]
-        auth.permit(:email, :password) if auth
+        auth&.permit(:email, :password)
+      end
+
+      def auth_headers(resource)
+        resource.create_new_auth_token
       end
     end
   end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,8 +1,9 @@
+# frozen_string_literal: true
+
 class UserSerializer < ActiveModel::Serializer
-  
   type 'user'
 
-  attributes :id, :email, :name, :country, :created_at, :updated_at
+  attributes :id, :email, :name, :country, :created_at, :updated_at, :token
 
   def name
     "#{object.first_name} #{object.last_name}"
@@ -14,5 +15,9 @@ class UserSerializer < ActiveModel::Serializer
 
   def updated_at
     object.updated_at.strftime('%Y-%m-%dT%H:%M%:z')
+  end
+
+  def token
+    @instance_options[:authorization_token]
   end
 end
